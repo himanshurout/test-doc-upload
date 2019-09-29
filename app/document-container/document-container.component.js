b9f924680ce918d85fb147ef0a91aae807d80608
Himanshu <himanshurout@yahoo.com>
2019-09-29 00:37:39 -0400

    use multiple doc upload comp using inputs

--- app/document-container/document-container.component.js
+++ app/document-container/document-container.component.js
@@ -1,14 +1,90 @@
 'use strict';
 
-// Register `documentContainer` component, along with its associated controller and template
+function documentContainerController() {
+  var ctrl = this;
+
+  ctrl.serologyDoc = {
+    columns : [
+      {
+        "key": "docType",
+        "value": "Document Type"
+      },
+      {
+        "key": "files",
+        "value": "Document Attached"
+      },
+      {
+        "key": "upload",
+        "value": "Attach Document"
+      }
+    ],
+    documentTypes : [
+      {
+        "docType": "Serology Report"
+      }
+    ]
+  };
+
+  ctrl.consolidatedDoc = {
+    columns : [
+      {
+        "key": "docType",
+        "value": "Document Type"
+      },
+      {
+        "key": "files",
+        "value": "Document Attached"
+      },
+      {
+        "key": "upload",
+        "value": "Attach Document"
+      }
+    ],
+    documentTypes : [
+      {
+        "docType": "Serology Report - Consolidated"
+      }
+    ]
+  };
+
+  ctrl.additionalDoc = {
+    columns : [
+      {
+        "key": "docType",
+        "value": "Document Type"
+      },
+      {
+        "key": "files",
+        "value": "Document Attached"
+      },
+      {
+        "key": "initiatedDate",
+        "value": "Initiated Date"
+      },
+      {
+        "key": "lapsedDays",
+        "value": "Lapsed Days"
+      },
+      {
+        "key": "upload",
+        "value": "Attach Document"
+      }
+    ],
+    documentTypes : [
+      {
+        "docType": "Serology Report",
+        "initiatedDate": "07/01/19",
+        "lapsedDays": "21"
+      }
+    ]
+  };
+
+
+}
+
 angular.
   module('documentContainer').
   component('documentContainer', {
     templateUrl: 'document-container/document-container.template.html',
-    controller: ['$routeParams',
-      function documentContainerController($routeParams, Phone) {
-        var self = this;
-      
-      }
-    ]
+    controller: documentContainerController
   });
--- app/document-uploader/document-uploader.component.js
+++ app/document-uploader/document-uploader.component.js
@@ -1,51 +1,16 @@
 'use strict';
 
-// Register `documentUploader` component, along with its associated controller and template
+function documentUploaderController() {
+  var ctrl = this;
+}
+
 angular.
   module('documentUploader').
   component('documentUploader', {
     templateUrl: 'document-uploader/document-uploader.template.html',
-    controller: ['$routeParams',
-      function documentUploaderController($routeParams, Phone) {
-        var self = this;
-        self.columns = [
-          {
-            "key": "docType",
-            "value": "Document Type"
-          },
-          {
-            "key": "files",
-            "value": "Document Attached"
-          },{
-            "key": "date",
-            "value": "Date"
-          },
-          {
-            "key": "date2",
-            "value": "Date2"
-          },
-          {
-            "key": "upload",
-            "value": "Attach Document"
-          }
-        ];
-
-
-        self.documentTypes = [
-          {
-            "docType": "Initial",
-            "date": "10/10/2019",
-            "date2": "17/10/2019"
-          },
-          {
-            "docType": "Attached",
-            "date": "10/10/2019"
-          },
-          {
-            "docType": "xyz",
-            "date": "10/10/2019"
-          }
-        ];
-      }
-    ]
+    controller: documentUploaderController,
+    bindings: {
+      columns: '<',
+      documentTypes: '<'
+    }
   });
